386cc60e1dc6162ff1c2918d4489c3316b0185fc
Jose Lee <jose.lee@superb.today>
2026-06-10 22:37:45 +0900

    style(shared): restore import ordering in battle screen

--- shared/src/commonMain/kotlin/today/superb/jvl/ui/battle/BattleScreen.kt
+++ shared/src/commonMain/kotlin/today/superb/jvl/ui/battle/BattleScreen.kt
@@ -25,9 +25,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.lerp
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
